3b3d6ac3be381f4c6e6d6498606eb2f11b3160c4
FatMeryl <kher.job@gmail.com>
2019-07-04 10:44:38 +0200

    Odpowiedzi do komentarzy

--- MainWindowRadka/mainWindow.cs
+++ MainWindowRadka/mainWindow.cs
@@ -27,9 +27,14 @@ namespace Sessions_Uploader
         {
             InitializeComponent();
             tempDirectory = $@"{tempFolderTekstBox.Text}\";
-			
-			/* createDirectory nie wywala się jak katalog już jest, więc można pominąc pisanie if Dir exists */
-			/* nie wiem na ile chcesz to mieć idiotoodporne, ale jak create rzuci ci wyjątkiem to chyba cały program się wysypie, bo nie masz try-catch */
+
+            /* createDirectory nie wywala się jak katalog już jest, więc można pominąc pisanie if Dir exists */
+            /* nie wiem na ile chcesz to mieć idiotoodporne, ale jak create rzuci ci wyjątkiem to chyba cały program się wysypie, bo nie masz try-catch */
+
+            /*
+			 Dodałem to aby przy 1wszym uruchomieniu stworzył się ten ten folder. W przypadku skasowania folderu przy już uruchomionym programie
+             mam obsłużony ten bug w commicie 9ce26d525bf5387191957e12ab28c88e08d8d30f
+			 */
             if (!Directory.Exists(tempDirectory))
             {
                 Directory.CreateDirectory(tempDirectory);
@@ -42,6 +47,12 @@ namespace Sessions_Uploader
 			/* jest cos takiego jak AddRange, więc stawiam, że możnaby napisać:
 				comboBoxServers.Items.AddRange(listOfServers.Keys)
 			*/
+            /*
+             Nie znam tego. To tujaj sam wykombinowałem foreachem.
+             AddRange bierze jako parametr tablicę objektów, musiał bym zmianiać wsztko niżej aby przrobić z Dictionary.
+             Czy warto?
+             */
+       
 				
             foreach (var key in listOfServers.Keys)
             {
@@ -58,8 +69,8 @@ namespace Sessions_Uploader
                 MessageBoxIcon.Asterisk);
         }
 
-		/* tutaj to mały kod i to nie ma większego znaczenia, ale generalnie nie podoba mi się, że to sprawdza czy wszystko jest ok, a do tego jeszcze wyświetla boxy, trzebaby rozdzielić odpowiedzialności
-			najprostszy pomysł jaki przychodzi mi do głowy to zrobić enuma w stylu
+		/* tutaj to mały kod i to nie ma większego znaczenia, ale generalnie nie podoba mi się, że to sprawdza czy wszystko jest ok,
+		 a do tego jeszcze wyświetla boxy, trzebaby rozdzielić odpowiedzialności najprostszy pomysł jaki przychodzi mi do głowy to zrobić enuma w stylu
 			enum state {
 				OK,
 				NoSourceDir,
@@ -68,6 +79,10 @@ namespace Sessions_Uploader
 			}
 			zwracać wartość takiego enuma, a następnie jakaś inna metoda nazywająca się GenerateMessage dostawałaby takiego enuma i wyświetlała odpowiednie okienko
 		*/
+
+        /*
+         Brzmi dla mnie jak większe zadanie, ale posiedzę nad tym i zobaczę co uda się zakodzić.
+         */
         private bool ValidateControls()
         {
             if (!Directory.Exists(directorySourceTextBox.Text))
@@ -82,6 +97,8 @@ namespace Sessions_Uploader
             }
 
 			/* Na pewno < 2 ? */
+            // Tak, 2 anny to minimum aby obliczyć czas interwału potrzebny do przestawienia czasu badania oraz jego ID.
+
             if (GetAnnFilesNames().Count() < 2)
             {
                 directorySourceTextBox.BackColor = Color.LightPink;
@@ -89,7 +106,9 @@ namespace Sessions_Uploader
 					/* generalnie zamiast pisać \n lepiej uzyc Environment.NewLine, a więc jakos tak:
 						"Source directory does not contains any .ann files." + Environment.NewLine + "Please provide valid directory."
 					*/
-					/* brakuje kropki miedzyt zdaniami */
+					/* brakuje kropki miedzy zdaniami */
+
+                    // O tego nie znałem, thx. I kropkę też dostawię.
                     "Source directory does not contains any .ann files\nPlease provide valid directory.",
                     "Information",
                     MessageBoxButtons.OK,
@@ -111,6 +130,8 @@ namespace Sessions_Uploader
             listOfServers.TryGetValue(comboBoxServers.Text, out serverpath);
 
 			/* ten string Examination Creator powinienes dac do jakiejs stałej i to jej uzywać */
+
+            // Czy dobrze kombinuję aby zrobić z tego pole i w konstruktorze przypisać wartość?
             if (!Directory.Exists(serverpath) && serverpath != "Examination Creator")
             {
                 MessageBox.Show(
@@ -164,8 +185,15 @@ namespace Sessions_Uploader
                 {
                     writer.WriteLine($"Examination no.{i}, ID = {CheckSelectedServer(listOfServers)}");
 
-					/* po co jest ten delay tutaj? */
-					/* + tak jak napisałeś czekasz po zakończeniu ostatniego o ile jest ich wiecej niż jeden, a zatem jesli to mają być przerwy pomiędzy zadaniami to lepiej napisać if (i > 1) */
+                    /* po co jest ten delay tutaj? */
+                    /* + tak jak napisałeś czekasz po zakończeniu ostatniego o ile jest ich wiecej niż jeden, a zatem jesli to mają być przerwy pomiędzy zadaniami to lepiej napisać if (i > 1) */
+
+                    /*
+                     W debugu to nie wyszło, ale jak odpaliłem w normalnym trybie
+                     program to przy tworzeniu 2+ badań to rzucało mi Exceptionem.
+                     Drugie badanie dostawało te same ID.
+                     Commit a053e4e4296ca75ae160d14da629232ae600fc56, nazwa commita niestety słaba :/
+                     */
                     if (howManyTimes.Value > 1)
                     {
                         Task.Delay(1000).Wait();
@@ -197,6 +225,8 @@ namespace Sessions_Uploader
         private string CheckSelectedServer(Dictionary<string, string> listOfServers)
         {
 			/* czy te kolejne nawiasy klamrowe to pozostałośc po czymś czy są potrzebne? */
+
+            // do skasowania, moje niedopatrzenie
             {
                 var uploader = new Uploader(
                     DateTime.Now.ToLocalTime() - TimeSpan.FromHours(int.Parse(calculatedInterval)),
@@ -216,6 +246,10 @@ namespace Sessions_Uploader
 
 		/* ta i następna metoda wyglądają tak samo praktycznie, fajnie by było się nie powtarzać
 			można zrobić kolejną dostającą text przez refa i wywołać raz z directorySourceTextBox.Text, a drugi z directoryOutputTextBox.Text */
+
+        /*
+         To już level dla mnie zaawansowany :), ale też poczytam i sprawdzę czy uda się zakodzić.
+         */
         private void BrowseSource_Click(object sender, EventArgs e)
         {
             directorySourceTextBox.BackColor = Color.Empty;
@@ -283,6 +317,13 @@ namespace Sessions_Uploader
 			/* mozna po prostu napsiać
 				return lastAnnDate - firstAnnDate).TotalHours;
 			*/
+
+            /*
+             Nie zastanawiałem się nad tym. A może nie wiedziałem, że można.
+             Czy to zadziała tak samo jak zwrócę timespana?
+
+            Co do returna, to rzeczywiście od razu lepiej retrun //kod
+             */
             var interval = (lastAnnDate - firstAnnDate).TotalHours;
             return interval;
         }
@@ -296,11 +337,25 @@ namespace Sessions_Uploader
                     .Where(s => s.EndsWith(".ann")).ToList();
 				*/
 				/* dlaczego robisz ToList? */
+
+                /*                 
+                 Return racja, jak wyżej.
+
+                Zwaracam listę ponieważ potem biorę z niej pierwszy i ostatni item i na podstawie ich czasów wyliczam interwał(czas trwania badania)
+                 */
                 var files = Directory.GetFiles(directorySourceTextBox.Text, "*", SearchOption.AllDirectories)
                     .Where(s => s.EndsWith(".ann")).ToList();
                 return files;
             }
-			/* trochę ogólny ten catch, wiesz jakie konkretnie wyjątki chcesz złapać i dlaczego moga wystąpić? */
+            /* trochę ogólny ten catch, wiesz jakie konkretnie wyjątki chcesz złapać i dlaczego moga wystąpić? */
+
+            /*
+            Chcę to łapać jeżeli user poda w source np C:, albo D:
+            Wyszło przypadkiem dla własnie takich lokalizacji jako source.
+            Jak podawałem foldery nie zawierające badań to program działał. 
+            Rzucało tylko właśnie przy C:, D: etc jeżeli ta lokalizacja nie zawierała .ann.
+            Commit 230b59f62010745c253e36cafd6c57b73c3b8e41
+             */
             catch (Exception)
             {
                 return new List<string>();
@@ -320,6 +375,15 @@ namespace Sessions_Uploader
             var interval = CalculateExaminationDurationRoundedToHours(files);
 
 			/* to trochę wraca do pytania wyzej o zwracanie timespana zamiast godzin, ale nie rozumiem dlaczego ten math.ceiling tutaj */
+
+            /*
+             Zaokrąglam interwał aby wynosił minumum 1 godzinę, dla badań gdzie wyliczony czas trwania wynosi mniej niż 1 godzinę.
+             Ten program tak już został przez Tomka napisany, że trzeba było podać z palca ile trwa badanie.
+             Ta wartość była odejmowana od aktualnego czasu i tak powstawał czas startu badania. Chodzi o to, aby badania nie były
+             z przysłości.
+             Ja sobie sam wykombiniwałem automatyczne wyliczenie interwału i zaookrąglam aby minuimum było to 1godzina, nawet
+             jak badanie trwało 5minut.
+             */
             return calculatedInterval = Math.Ceiling(interval).ToString();
         }
 
@@ -328,6 +392,11 @@ namespace Sessions_Uploader
 			/* można po prostu napisać:
 				tempFolderTekstBox.Enabled = tempFolderCheckBox.Checked;
 			*/
+            
+            /*
+             No tak :). Properta Checked ustawia boola. 
+             Wychodzi moje myślenie "łopatologiczne".
+             */
             if (tempFolderCheckBox.Checked)
             {
                 tempFolderTekstBox.Enabled = true;
@@ -343,6 +412,7 @@ namespace Sessions_Uploader
 			/* mozna po prostu napisać
 				OutputGroupBox.Visible = comboBoxServers.Text == "Examination Creator";
 			*/
+            // j.w.
             if (comboBoxServers.Text == "Examination Creator")
             {
                 OutputGroupBox.Visible = true;
@@ -361,7 +431,9 @@ namespace Sessions_Uploader
             }
             else
             {
-				/* trochę niekonsekwencja -  wcześniej w openTmp czy coś takiego miałeś to w oddzielnej metodzie */
+                /* trochę niekonsekwencja -  wcześniej w openTmp czy coś takiego miałeś to w oddzielnej metodzie */
+
+                // Chodzi o MsgTempDirectoryNotExist() ? Jezeli tak, to zrobiłem na to metodkę, bo wykorzystuję to w 2-óch miejscach.
                 MessageBox.Show(
                     "Config file does not exist",
                     "Information",
@@ -375,6 +447,8 @@ namespace Sessions_Uploader
 			/* lepiej zamiast / uzyć Path.PathSeparator, a więc:
 				tempFolderTekstBox.Text + Path.PathSeparator
 			*/
+
+            // nowość dla mnie, dzieki
             tempDirectory = $@"{tempFolderTekstBox.Text}\";
         }
 
