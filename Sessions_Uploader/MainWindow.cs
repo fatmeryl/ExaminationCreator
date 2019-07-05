b541c1354c3db2e20cbaa089aa73e7ffb84ae888
FatMeryl <kher.job@gmail.com>
2019-07-05 13:29:47 +0200

    Started to refactor code following Radek's comments

--- Sessions_Uploader/MainWindow.cs
+++ Sessions_Uploader/MainWindow.cs
@@ -13,6 +13,8 @@ namespace Sessions_Uploader
 {
     public partial class MainWindow : Form
     {
+        private const string ExaminationCreator = "Examination Creator";
+
         private readonly string configPath;
 
         private readonly IServerConfigurationProvider serverConfigurationProvider;
@@ -26,7 +28,7 @@ namespace Sessions_Uploader
         public MainWindow()
         {
             InitializeComponent();
-            tempDirectory = $@"{tempFolderTekstBox.Text}\";
+            tempDirectory = tempFolderTekstBox.Text + Path.DirectorySeparatorChar;
             if (!Directory.Exists(tempDirectory))
             {
                 Directory.CreateDirectory(tempDirectory);
@@ -70,7 +72,8 @@ namespace Sessions_Uploader
             {
                 directorySourceTextBox.BackColor = Color.LightPink;
                 MessageBox.Show(
-                    "Source directory does not contains any .ann files\nPlease provide valid directory.",
+                    "Source directory does not contains any .ann files" +
+                    Environment.NewLine + "Please provide valid directory.",
                     "Information",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Asterisk);
@@ -90,17 +93,18 @@ namespace Sessions_Uploader
             string serverpath;
             listOfServers.TryGetValue(comboBoxServers.Text, out serverpath);
 
-            if (!Directory.Exists(serverpath) && serverpath != "Examination Creator")
+            if (!Directory.Exists(serverpath) && serverpath != ExaminationCreator)
             {
                 MessageBox.Show(
-                    "There was a problem with connection to selected server.\nCheck your network connection.",
+                    "There was a problem with connection to selected server." +
+                    Environment.NewLine + "Check your network connection.",
                     "Connection to server problem",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Asterisk);
                 return false;
             }
 
-            if (serverpath == "Examination Creator")
+            if (serverpath == ExaminationCreator)
             {
                 if (directoryOutputTextBox.Text == directorySourceTextBox.Text)
                 {
@@ -141,7 +145,9 @@ namespace Sessions_Uploader
                 return;
             }
 
-            var logFilePath = $@"{tempDirectory}\Session Uploader Log {DateTime.Now:yyyy-MM-dd HHmmss}.txt";
+            //var logFilePath = $@"{tempDirectory}\Session Uploader Log {DateTime.Now:yyyy-MM-dd HHmmss}.txt";
+            var logFilePath = Path.Combine(tempDirectory,
+                $"Session Uploader Log {DateTime.Now:yyyy-MM-dd HHmmss}.txt");
             using (var writer = File.CreateText(logFilePath))
             {
                 for (int i = 1; i <= howManyTimes.Value; i++)
@@ -161,7 +167,7 @@ namespace Sessions_Uploader
             }
 
             MessageBox.Show(
-                "Session(s) successfully uploaded!\n",
+                "Session(s) successfully uploaded!",
                 "Information",
                 MessageBoxButtons.OK,
                 MessageBoxIcon.Asterisk);
@@ -178,21 +184,19 @@ namespace Sessions_Uploader
 
         private string CheckSelectedServer(Dictionary<string, string> listOfServers)
         {
+            var uploader = new Uploader(
+                DateTime.Now.ToLocalTime() - TimeSpan.FromHours(int.Parse(calculatedInterval)),
+                $@"{directorySourceTextBox.Text}\",
+                tempDirectory);
+
+            listOfServers.TryGetValue(comboBoxServers.Text, out var serverPath);
+
+            if (serverPath == ExaminationCreator)
             {
-                var uploader = new Uploader(
-                    DateTime.Now.ToLocalTime() - TimeSpan.FromHours(int.Parse(calculatedInterval)),
-                    $@"{directorySourceTextBox.Text}\",
-                    tempDirectory);
-
-                listOfServers.TryGetValue(comboBoxServers.Text, out var serverPath);
-
-                if (serverPath == "Examination Creator")
-                {
-                    serverPath = directoryOutputTextBox.Text;
-                }
-
-                return uploader.UploadToServer(serverPath);
+                serverPath = directoryOutputTextBox.Text;
             }
+
+            return uploader.UploadToServer(serverPath);
         }
 
         private void BrowseSource_Click(object sender, EventArgs e)
@@ -235,32 +239,41 @@ namespace Sessions_Uploader
             {
                 directorySourceTextBox.BackColor = Color.LightPink;
                 MessageBox.Show(
-                    "Source directory does not contains any .ann files\nPlease provide valid directory.",
+                    "Source directory does not contains any .ann files" +
+                    Environment.NewLine + "Please provide valid directory.",
                     "Information",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Asterisk);
                 return 0;
             }
 
-            var firstAnn = new SessionId(Path.GetFileNameWithoutExtension(files.First()));
-            var lastAnn = new SessionId(Path.GetFileNameWithoutExtension(files.Last()));
+            //var firstAnn = new SessionId(Path.GetFileNameWithoutExtension(files.First()));
+            //var lastAnn = new SessionId(Path.GetFileNameWithoutExtension(files.Last()));
+            //
+            //var firstAnnDate = DateTime.ParseExact(firstAnn.GetDate(), "yyyyMMddHHmmss", CultureInfo.InvariantCulture)
+            //    .ToUniversalTime();
+            //var lastAnnDate = DateTime.ParseExact(lastAnn.GetDate(), "yyyyMMddHHmmss", CultureInfo.InvariantCulture)
+            //    .ToUniversalTime();
+            //
+            //var interval = (lastAnnDate - firstAnnDate).TotalHours;
+            //return interval;
 
-            var firstAnnDate = DateTime.ParseExact(firstAnn.GetDate(), "yyyyMMddHHmmss", CultureInfo.InvariantCulture)
-                .ToUniversalTime();
-            var lastAnnDate = DateTime.ParseExact(lastAnn.GetDate(), "yyyyMMddHHmmss", CultureInfo.InvariantCulture)
-                .ToUniversalTime();
+            return (this.GetDate(files.Last()) - this.GetDate(files.First())).TotalHours;
 
-            var interval = (lastAnnDate - firstAnnDate).TotalHours;
-            return interval;
+        }
+
+        private DateTime GetDate(string filename)
+        {
+            var ann = new SessionId(Path.GetFileNameWithoutExtension(filename));
+            return DateTime.ParseExact(ann.GetDate(), "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
         }
 
         private IEnumerable<string> GetAnnFilesNames()
         {
             try
             {
-                var files = Directory.GetFiles(directorySourceTextBox.Text, "*", SearchOption.AllDirectories)
-                    .Where(s => s.EndsWith(".ann")).ToList();
-                return files;
+                return Directory.GetFiles(directorySourceTextBox.Text, "*", SearchOption.AllDirectories)
+                    .Where(s => s.EndsWith(".ann"));
             }
             catch (Exception)
             {
@@ -287,26 +300,12 @@ namespace Sessions_Uploader
         {
             tempFolderTekstBox.BackColor = Color.Empty;
 
-            if (tempFolderCheckBox.Checked)
-            {
-                tempFolderTekstBox.Enabled = true;
-            }
-            else
-            {
-                tempFolderTekstBox.Enabled = false;
-            }
+            tempFolderTekstBox.Enabled = tempFolderCheckBox.Checked;
         }
 
         private void comboBoxServers_TextChanged(object sender, EventArgs e)
         {
-            if (comboBoxServers.Text == "Examination Creator")
-            {
-                OutputGroupBox.Visible = true;
-            }
-            else
-            {
-                OutputGroupBox.Visible = false;
-            }
+            OutputGroupBox.Visible = comboBoxServers.Text == ExaminationCreator;
         }
 
         private void openConfigBtn_Click(object sender, EventArgs e)
@@ -327,7 +326,7 @@ namespace Sessions_Uploader
 
         private void tempFolderTextBox_TextChanged(object sender, EventArgs e)
         {
-            tempDirectory = $@"{tempFolderTekstBox.Text}\";
+            tempDirectory = tempFolderTekstBox.Text + Path.DirectorySeparatorChar;
         }
 
         private void HowManyTimes_KeyPress(object sender, KeyPressEventArgs e)
