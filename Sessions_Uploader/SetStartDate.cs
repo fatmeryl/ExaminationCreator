e8917dd7123350454fc7011967a58b123e23fd34
fatmeryl <fatmeryl>
2021-03-16 20:10:08 +0100

    Get date from picker works

--- Sessions_Uploader/SetStartDate.cs
+++ Sessions_Uploader/SetStartDate.cs
@@ -8,12 +8,11 @@ namespace ExaminationCreator
         public static DateTime GetSelectedTimeAndDate(string picketDate)
         {
             var time = DateTime.Now.ToString("HH:mm:ss");
-            var date = picketDate;
-            var space = " ";
-            DateTime myDate = DateTime.ParseExact($"{date}{space}{time}", "dd.MM.yyyy HH:mm:ss",
+            DateTime myDate = DateTime.ParseExact($"{picketDate} {time}", "dd.MM.yyyy HH:mm:ss",
                 CultureInfo.CurrentCulture, DateTimeStyles.None);
-            var test = DateTime.Now.ToLocalTime();
-            return DateTime.Now.ToLocalTime();
+            return myDate;
+            //var test = DateTime.Now.ToLocalTime();
+            //return DateTime.Now.ToLocalTime();
         }
     }
 }
